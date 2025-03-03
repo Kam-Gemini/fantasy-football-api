65f5c2bed1897f87b455ae8bbd287baf9d8b5aca
Kamran <kamran.raja@hotmail.co.uk>
2025-03-03 21:39:58 +0000

    fav team and fantasy team name updated

--- users/serializers/common.py
+++ users/serializers/common.py
@@ -2,16 +2,18 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model, password_validation, hashers
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password_confirmation = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=False)  # Make password optional
+    password_confirmation = serializers.CharField(write_only=True, required=False)  # Make password_confirmation optional
 
     def validate(self, data):
-        password = data.pop('password')
-        password_confirmation = data.pop('password_confirmation')
-        if password != password_confirmation:
-            raise serializers.ValidationError({'password': 'Passwords do not match'})
-        password_validation.validate_password(password)
-        data['password'] = hashers.make_password(password)
+        # Only validate password if it's being updated
+        if 'password' in data:
+            password = data.pop('password')
+            password_confirmation = data.pop('password_confirmation', None)  # Use .pop() with a default to avoid KeyError
+            if password != password_confirmation:
+                raise serializers.ValidationError({'password': 'Passwords do not match'})
+            password_validation.validate_password(password)
+            data['password'] = hashers.make_password(password)
         return data
 
     class Meta:
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisteredView, LoginView
+from .views import RegisteredView, LoginView, UserProfileView
 
 urlpatterns = [
     path('register/', RegisteredView.as_view()),
+    path('profile/', UserProfileView.as_view()),
     path('login/', LoginView.as_view())
 ]
