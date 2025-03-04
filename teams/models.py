02dc268700d138887f9e516bf0259de3c8df90fb
Kamran <kamran.raja@hotmail.co.uk>
2025-03-04 14:39:49 +0000

    updated players model for images

--- teams/models.py
+++ teams/models.py
@@ -7,9 +7,9 @@ from users.models import User
 class Teams(models.Model):
     team_name = models.CharField(max_length=50)
     goalkeeper = models.ForeignKey(to=Players, on_delete=models.SET_NULL, related_name='teams_as_goalkeeper', null=True, blank=True)
-    defenders = models.ManyToManyField(to=Players, related_name='team_defenders', null=True, blank=True)
-    midfielders = models.ManyToManyField(to=Players, related_name='team_midfielders', null=True, blank=True)
-    forwards = models.ManyToManyField(to=Players, related_name='team_forwards', null=True, blank=True)
+    defenders = models.ManyToManyField(to=Players, related_name='team_defenders', blank=True)
+    midfielders = models.ManyToManyField(to=Players, related_name='team_midfielders', blank=True)
+    forwards = models.ManyToManyField(to=Players, related_name='team_forwards', blank=True)
     total_cost = models.IntegerField(blank=True, null=True)
     total_points = models.IntegerField(blank=True, null=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='own_team', blank=True, null=True)
